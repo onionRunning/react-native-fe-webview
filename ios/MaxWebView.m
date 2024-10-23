1730112f2ea78be9434246bc062bde30eb4dc009
CBH <243671403@qq.com>
2024-10-23 11:49:26 +0800

    remove test code

--- ios/MaxWebView.m
+++ ios/MaxWebView.m
@@ -36,9 +36,7 @@
 
 - (void)setUrl:(NSString *)url {
     _url = url;
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-      [self loadURL];
-    });
+    [self loadURL];
 }
 
 - (void)loadURL {
