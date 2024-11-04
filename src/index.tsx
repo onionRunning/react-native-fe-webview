b65f68c6be0d2c39df32207117caa0b359b15edb
wangcong <wangcong@mampod.com>
2024-11-04 16:44:36 +0800

    fix: remove console

--- src/index.tsx
+++ src/index.tsx
@@ -24,7 +24,7 @@ const ExWebView = React.forwardRef((props: Props, ref) => {
 
   // 可以发给H5 定义类型 和参数 
   const onPostMessage = (payload: {type: string, params: any}) => {
-    // console.warn('我执行了么', payload)
+    // console.warn('我执行了么222222', payload)
     UIManager?.dispatchViewManagerCommand?.(
       findNodeHandle?.(webviewRef?.current),
       UIManager?.getViewManagerConfig?.('MaxWebView')?.Commands?.processData!,
@@ -36,7 +36,7 @@ const ExWebView = React.forwardRef((props: Props, ref) => {
     if (webviewRef.current) {
       UIManager?.dispatchViewManagerCommand?.(
         findNodeHandle?.(webviewRef.current),
-        UIManager.getViewManagerConfig('CustomWebView').Commands.closeKeyboard!,
+        UIManager.getViewManagerConfig('MaxWebView').Commands.closeKeyboard!,
         [],
       )
     }
@@ -45,14 +45,14 @@ const ExWebView = React.forwardRef((props: Props, ref) => {
   const openKeyBoard = () => {
     UIManager.dispatchViewManagerCommand(
       findNodeHandle(webviewRef.current),
-      UIManager.getViewManagerConfig('CustomWebView').Commands.openKeyboard!,
+      UIManager.getViewManagerConfig('MaxWebView').Commands.openKeyboard!,
       [],
     )
   }
 
   return (
     <MaxWebView 
-      ref={(f: any) => (webviewRef.current = f)} 
+      ref={webviewRef} 
       style={style} 
       onEventChange={onEventMessage} 
       url={url}
